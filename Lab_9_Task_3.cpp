405391adb9303a9f50b150cafc28942288f6ea33
Zain Wajid <u2023775@giki.edu.pk>
2024-12-03 12:37:27 +0500

    Lab 9

--- Lab_9_Task_3.cpp
+++ Lab_9_Task_3.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 // Node class
-class Node {
+class Node 
+{
 public:
     int data;
     int height;
@@ -12,46 +13,45 @@ public:
     Node(int val) : data(val), height(1), left(nullptr), right(nullptr) {}
 };
 
-// AVL Tree class
 class AVLTree {
 private:
-    // Get the height of a node
-    int getHeight(Node* node) {
+    int getHeight(Node* node) 
+    {
         return node ? node->height : 0;
     }
 
     // Update the height of a node
-    void updateHeight(Node* node) {
-        if (node) {
+    void updateHeight(Node* node) 
+    {
+        if (node) 
+        {
             int leftHeight = getHeight(node->left);
             int rightHeight = getHeight(node->right);
             node->height = max(leftHeight, rightHeight) + 1;
         }
     }
 
-    // Get the balance factor of a node
-    int getBalanceFactor(Node* node) {
+    int getBalanceFactor(Node* node) 
+    {
         return node ? getHeight(node->left) - getHeight(node->right) : 0;
     }
 
-    // Left rotation
-    Node* rotateLeft(Node* x) {
+    Node* rotateLeft(Node* x) 
+    {
         Node* y = x->right;
         Node* T2 = y->left;
 
-        // Perform rotation
         y->left = x;
         x->right = T2;
 
-        // Update heights
         updateHeight(x);
         updateHeight(y);
 
         return y;
     }
 
-    // Right rotation
-    Node* rotateRight(Node* y) {
+    Node* rotateRight(Node* y) 
+    {
         Node* x = y->left;
         Node* T2 = x->right;
 
@@ -66,48 +66,45 @@ private:
         return x;
     }
 
-    // Rebalance the node
-    Node* rebalance(Node* node) {
+    Node* rebalance(Node* node) 
+    {
         int balanceFactor = getBalanceFactor(node);
 
-        // Left heavy
-        if (balanceFactor > 1) {
-            if (getBalanceFactor(node->left) < 0) {
-                // Left-Right case
+        if (balanceFactor > 1) 
+        {
+
+            if (getBalanceFactor(node->left) < 0) 
+            {
                 node->left = rotateLeft(node->left);
             }
-            // Left-Left case
             return rotateRight(node);
         }
 
-        // Right heavy
-        if (balanceFactor < -1) {
-            if (getBalanceFactor(node->right) > 0) {
-                // Right-Left case
+        if (balanceFactor < -1) 
+        {
+            if (getBalanceFactor(node->right) > 0) 
+            {
                 node->right = rotateRight(node->right);
             }
-            // Right-Right case
             return rotateLeft(node);
         }
 
         return node;
     }
 
-    // Recursive function to rebalance the entire tree
-    Node* rebalanceTree(Node* root) {
+    Node* rebalanceTree(Node* root) 
+    {
         if (!root) return nullptr;
 
-        // Rebalance the left and right subtrees
         root->left = rebalanceTree(root->left);
         root->right = rebalanceTree(root->right);
 
-        // Rebalance the current node
         return rebalance(root);
     }
-
-    // Recursive function to print the tree (in-order traversal)
-    void inOrder(Node* root) {
-        if (root) {
+    void inOrder(Node* root) 
+    {
+        if (root) 
+        {
             inOrder(root->left);
             cout << "Node: " << root->data << ", Balance Factor: " << getBalanceFactor(root) << endl;
             inOrder(root->right);
@@ -115,25 +112,25 @@ private:
     }
 
 public:
+
     Node* root;
 
     AVLTree() : root(nullptr) {}
 
-    // Wrapper for rebalanceTree
-    void rebalanceTree() {
+    void rebalanceTree() 
+    {
         root = rebalanceTree(root);
     }
-
-    // Function to print the tree
-    void printTree() {
+    void printTree() 
+    {
         inOrder(root);
     }
 };
 
-int main() {
+int main() 
+{
     AVLTree tree;
 
-    // Creating the BST resulting from Task 2 (after removing 5 without rebalancing)
     tree.root = new Node(4);
     tree.root->left = new Node(3);
     tree.root->left->left = new Node(2);
@@ -146,12 +143,12 @@ int main() {
     tree.root->right->right = new Node(11);
     tree.root->right->right->right = new Node(12);
 
-    cout << "Tree before rebalancing:\n";
     tree.printTree();
 
-    // Task 3: Rebalance the tree
     tree.rebalanceTree();
-    cout << "\nTree after rebalancing:\n";
+
+    cout << endl;
+    
     tree.printTree();
 
     return 0;
